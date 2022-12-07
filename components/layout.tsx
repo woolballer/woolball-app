331ccce49b31415cbc16d8843342d371ae61e666
tomlightning <le.tomer@gmail.com>
2022-12-07 13:35:24 +0200

    meta tags

--- components/layout.tsx
+++ components/layout.tsx
@@ -26,6 +26,38 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <Head>
         <title>Woolball a name system with links</title>
         <link rel="icon" href="/favicons/favicon.ico" />
+        <meta name="title" content="Woolball a name system with links" />
+        <meta
+          name="description"
+          content="Woolball is a Name System where names can link to each other.  Woolball is an extension to the subdomain system, everything you can do with subdomains, you can do with links."
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://woolball.xyz/" />
+        <meta property="og:title" content="Woolball a name system with links" />
+        <meta
+          property="og:description"
+          content="Woolball is a Name System where names can link to each other.  Woolball is an extension to the subdomain system, everything you can do with subdomains, you can do with links."
+        />
+        <meta
+          property="og:image"
+          content="https://woolball.xyz/images/screenshot.jpg"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://woolball.xyz/" />
+        <meta
+          property="twitter:title"
+          content="Woolball a name system with links"
+        />
+        <meta
+          property="twitter:description"
+          content="Woolball is a Name System where names can link to each other.  Woolball is an extension to the subdomain system, everything you can do with subdomains, you can do with links."
+        />
+        <meta
+          property="twitter:image"
+          content="https://woolball.xyz/images/screenshot.jpg"
+        ></meta>
       </Head>
 
       <main className={AspektaFont.className}>
